00eec163f58c455892484b45eb337bae82fe654b
Fergal Cotter <fbcotter90@gmail.com>
2016-11-04 00:08:50 +0000

    Created some machine specific fish config files

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,10 @@
+# Install Fundle Package manager
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+
+# Install some packages
+# Start ssh agent automatically
+fundle plugin 'tuvistavie/fish-ssh-agent'
+
 # Path to Oh My Fish install.
 set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
@@ -5,14 +12,28 @@ set -q XDG_DATA_HOME
 
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
-set PATH /usr/local/bin/ $PATH
-set PATH ~/.local/bin $PATH
-set PATH /usr/local/go/bin $PATH
 
-export GOPATH=/home/fbc23/code/go
-set PATH $GOPATH/bin $PATH
+# Some agnoster settings -> display user@host
+set -g theme_display_user yes
+set -g theme_hide_hostname no
 
-alias rm "echo Use 'trash', 'del' or the full path i.e. '/bin/rm'"
-alias ipy "python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
-alias ipy3 "python3 -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
-alias gitlog "git log --graph --decorate --oneline"
+# Turn on vim bindings
+if fish -v | grep 2.2 > /dev/null 
+    fish_vi_mode
+else
+    fish_vi_key_bindings
+end
+
+if [ (hostname) = "hazza" ]
+    if test -f $HOME/repos/fbcotter/dotfiles/fish/alias_vars_hazza.fish
+        source $HOME/repos/fbcotter/dotfiles/fish/alias_vars_hazza.fish
+    end
+else if [ (hostname) = "yoshi" ]
+    if test -f $HOME/repos/fbcotter/dotfiles/fish/alias_vars_yoshi.fish
+        source $HOME/repos/fbcotter/dotfiles/fish/alias_vars_yoshi.fish
+    end
+else if [ (hostname) = "glen" ]
+    if test -f $HOME/repos/fbcotter/dotfiles/fish/alias_vars_glen.fish
+        source $HOME/repos/fbcotter/dotfiles/fish/alias_vars_glen.fish
+    end
+end
