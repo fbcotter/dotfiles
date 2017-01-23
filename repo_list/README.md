bba668d749878021d1fbedb67a39b6f5a67ea500
Fergal Cotter <fbcotter90@gmail.com>
2017-01-23 13:02:37 +0000

    Created new folder "repo_list"
    repo_list keeps a list of all the repositories on my machines.
    Makes it easier to remember what to download in future.

---
+++ repo_list/README.md
@@ -0,0 +1,5 @@
+In here I keep a list of all the repos by host. That way, I can easily
+redownload them if something goes wrong.
+
+You can also run update_lists.sh to automatically update the list for the host
+you're currently using.
---
+++ repo_list/update_lists.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get the directory of the current file. Three levels up from this
+# will be the repos base directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR2="$DIR/../../.."
+
+# Make a link to the alias and environment variables file
+HOSTNAME=$(hostname)
+case $HOSTNAME in
+    (yoshi) 
+        echo "Host detected as yoshi. Creating the yoshi repo list"
+        find $DIR2 -depth 2 -type d | sed "s#$DIR2/##" > "$HOSTNAME"_repos.txt;;
+    (luigi) 
+        echo "Host detected as luigi. Creating the luigi repo list"
+        find $DIR2 -depth 2 -type d | sed "s#$DIR2/##" > "$HOSTNAME"_repos.txt;;
+    (Glen) 
+        echo "Host detected as Glen. Creating the Glen repo list"
+        find $DIR2 -depth 2 -type d | sed "s#$DIR2/##" > "$HOSTNAME"_repos.txt;;
+    (hazza) 
+        echo "Host detected as hazza. Creating the hazza repo list"
+        find $DIR2 -depth 2 -type d | sed "s#$DIR2/##" > "$HOSTNAME"_repos.txt;;
+    (*) 
+        echo "Warning - Unkown Hostname."  
+esac
+
+
