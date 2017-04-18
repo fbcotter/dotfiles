771f18016d879acc394d39900d2b64e8340884ce
Fergal Cotter <fbcotter90@gmail.com>
2017-04-18 23:19:44 +0100

    Added check for LC_COLORS environment variable
    This is to allow me to set whether the shell is solarized dark or light in only
    one place, and everything else should follow.

--- fish/config.fish
+++ fish/config.fish
@@ -3,20 +3,8 @@ if not status --is-interactive
     exit # Skips the rest of this file, not exiting the shell
 end
 
-# Install Fundle Package manager
-if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
-
-# Install some packages
-# Start ssh agent automatically
-#fundle plugin 'tuvistavie/fish-ssh-agent'
-# Start an ssh-agent (manual option for 
-# tuvistatvie/fish-ssh-agent)
 if test -z "$SSH_ENV"
     setenv SSH_ENV $HOME/.ssh/environment
-end
-
-if not __ssh_agent_is_started
-    __ssh_agent_start
 end
 
 # Path to Oh My Fish install.
@@ -28,9 +16,20 @@ set -q XDG_DATA_HOME
 source $OMF_PATH/init.fish
 
 # Some agnoster settings -> display user@host
-set -g theme_display_user yes
-set -g theme_hide_hostname no
+# set -g theme_display_user yes
+# set -g theme_hide_hostname no
 
+# Some bobthefish settings 
+set -gx theme_nerd_fonts yes
+set -gx theme_display_ruby no
+set -gx theme_display_virtualenv yes
+set -gx theme_display_user yes
+# if the environment variable "COLORS" is set, set the scheme to light
+if [ $LC_COLORS -a $LC_COLORS = "light" ] 
+    set -gx theme_color_scheme solarized-light
+else
+    set -gx theme_color_scheme solarized-dark
+end
 
 # Turn on vim bindings
 # fish -v outputted to stderr on some of my machines??
