762280cfd9b40f462298713fdadb8f7172ec6c48
Fergal Cotter <fbcotter90@gmail.com>
2017-04-19 11:01:18 +0100

    Added latex to path

--- fish/alias_vars_glen.fish
+++ fish/alias_vars_glen.fish
@@ -4,4 +4,6 @@ alias ipy "python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance
 alias ipy3 "python3 -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
 
 # Environment variables
-set -gx PATH $PATH ~/.local/bin
+set -gx PATH $PATH ~/.local/bin /usr/local/texlive/2016/bin/x86_64-linux
+# set -gx MANPATH /usr/local/texlive/2016/texmf-dist/doc/man /usr/local/man
+set -gx INFOPATH $INFOPATH /usr/local/texlive/2016/texmf-dist/doc/info
