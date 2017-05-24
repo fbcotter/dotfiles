1c10f387b3f39f81cdb611bcf8bafa43014728b1
Fergal Cotter <fbcotter90@gmail.com>
2017-05-24 23:44:41 +0100

    Added pyenv and updated repo list for glen

--- fish/alias_vars_glen.fish
+++ fish/alias_vars_glen.fish
@@ -4,6 +4,9 @@ alias ipy "python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance
 alias ipy3 "python3 -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
 
 # Environment variables
-set -gx PATH $PATH ~/.local/bin /usr/local/texlive/2016/bin/x86_64-linux
+set -gx PYENV_ROOT "$HOME/.pyenv"
+set -gx PATH $PYENV_ROOT/bin ~/.local/bin /usr/local/texlive/2016/bin/x86_64-linux $PATH
 # set -gx MANPATH /usr/local/texlive/2016/texmf-dist/doc/man /usr/local/man
 set -gx INFOPATH $INFOPATH /usr/local/texlive/2016/texmf-dist/doc/info
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
