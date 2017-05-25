00b21b03cd2922a909ad3e7b78a102a54c35f693
Fergal Cotter <fbcotter90@gmail.com>
2017-05-25 15:38:42 +0100

    Added pyenv to mario

--- fish/alias_vars_mario.fish
+++ fish/alias_vars_mario.fish
@@ -1,4 +1,7 @@
 # Set some environment variables
+set -gx PYENV_ROOT "$HOME/.pyenv"
 set -gx CUDA_HOME /usr/local/cuda-8.0
-set -gx PATH $CUDA_HOME/bin/ $PATH
+set -gx PATH $PYENV_ROOT/bin $CUDA_HOME/bin/ $PATH
 set -gx LD_LIBRARY_PATH "$LD_LIBRARY_PATH:$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64:$HOME/code/cuda8.0/cuda/lib64"
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
