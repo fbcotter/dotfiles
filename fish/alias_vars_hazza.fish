99877de1e2c1e73f8dfec5cb8c146875f81a59ae
Fergal Cotter <fbcotter90@gmail.com>
2017-06-02 12:53:56 +0100

    Updated vimrc file
    Added new ignores for flake8

--- fish/alias_vars_hazza.fish
+++ fish/alias_vars_hazza.fish
@@ -1,7 +1,10 @@
-set -gx PATH /usr/local/bin /Developer/NVIDIA/CUDA-8.0/bin $PATH
 set -gx DYLD_LIBRARY_PATH "/usr/local/cuda/lib:/Developer/NVIDIA/CUDA-8.0/lib:$DYLD_LIBRARY_PATH"
 #set -gx PATH ~/anaconda3/bin $PATH
 #source (conda info --root)/bin/conda.fish
+set -gx PYENV_ROOT "$HOME/.pyenv"
+set -gx PATH /usr/local/bin /Developer/NVIDIA/CUDA-8.0/bin $PATH
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
 alias rm "echo Use 'trash', 'del' or the full path i.e. '/bin/rm'"
 alias ipy "python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
 alias ipy3 "python3 -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
