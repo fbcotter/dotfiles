080fb5d2c32f2f3d7d3c8fe043c2e9b0f1dda561
Fergal Cotter <fbcotter90@gmail.com>
2017-07-25 14:37:38 +0100

    Changed matplotlib backend to tkagg

--- fish/alias_vars_glen.fish
+++ fish/alias_vars_glen.fish
@@ -1,5 +1,5 @@
 # Aliases
-alias rm "echo Use 'trash', 'del' or the full path i.e. '/bin/rm'"
+# alias rm "echo Use 'trash', 'del' or the full path i.e. '/bin/rm'"
 alias ipy "python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
 alias ipy3 "python3 -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
 
