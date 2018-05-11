b15fbfee19f4ca4da6a7b2553b7e20d879d1f148
Fergal Cotter <fbcotter90@gmail.com>
2018-05-11 14:17:52 +0100

    Updated CUDA paths

--- fish/alias_vars_yoshi.fish
+++ fish/alias_vars_yoshi.fish
@@ -1,8 +1,7 @@
 # Set some environment variables
-set -gx CUDA_HOME /usr/local/cuda-8.0
+set -gx CUDA_HOME /usr/local/cuda
 set -gx LD_LIBRARY_PATH "$LD_LIBRARY_PATH:$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64"
-set -gx LD_LIBRARY_PATH "$LD_LIBRARY_PATH:$HOME/code/cuda8.0/cudnn5.1/lib64"
-set -gx LD_LIBRARY_PATH "$LD_LIBRARY_PATH:$HOME/code/cuda8.0/cudnn6.0/lib64"
+set -gx LD_LIBRARY_PATH "$LD_LIBRARY_PATH:$HOME/code/cuda9.1/cudnn7.0/lib64"
 set -gx DOCKER_ID_USER "fbcotter"
 set -gx HOME "/scratch/fbc23"
 set -gx PYENV_ROOT "$HOME/.pyenv"
