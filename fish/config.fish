98d161b4b56667ee90b487115f1ca185fcf66548
Fergal Cotter <fbcotter90@gmail.com>
2020-01-03 11:14:54 +0000

    Updated config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -8,83 +8,21 @@ if test -z "$SSH_ENV"
 end
 
 # Start ssh agent
-if not __ssh_agent_is_started
-    __ssh_agent_start
-end
-
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+# if not __ssh_agent_is_started
+    # __ssh_agent_start
+# end
 
 # Some agnoster settings -> display user@host
 set -g theme_display_user yes
 set -g theme_hide_hostname no
 
-# Some bobthefish settings 
-# set -gx theme_nerd_fonts yes
-# set -gx theme_display_ruby no
-# set -gx theme_project_dir_length 1 
-# set -gx fish_prompt_pwd_dir_length 1 
-# set -gx theme_display_virtualenv yes
-# set -gx theme_display_user yes
-# set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
-# # if the environment variable "COLORS" is set, set the scheme to light
-# if [ $LC_COLORS -a $LC_COLORS = "light" ] 
-    # set -gx theme_color_scheme solarized-light
-# else
-    # set -gx theme_color_scheme solarized-dark
-# end
-
-# Turn on vim bindings
-# fish -v outputted to stderr on some of my machines??
-# Hence the 2>&1
-# if fish -v 2>&1 | grep 2.[0-2] > /dev/null 
-    # fish_vi_mode
-# else
-    # fish_vi_key_bindings
-# end
-
 # Some aliases/environment variables
-set -gx EDITOR vim
-alias gitlog "git log --graph --decorate --oneline --all"
-alias cp "cp -i"
-alias mv "mv -i"
-alias latexmk "latexmk -pdf -pvc"
-alias gtree "tree -a -I '.git|venv|*.egg-info|build|.cache|__pycache__|.ipynb_checkpoints|dist'"
-alias mymatlab "set -gx SHELL /bin/bash; matlab"
-alias mymatlab_shell "set -gx SHELL /bin/bash; matlab -nosplash -nodesktop"
-alias cords pip
-source $HOME/.config/fish/less_colours.fish
-set -gx TERM xterm-256color
-set -gx PATH $PATH $HOME/.local/bin
-set -gx TEXINPUTS ".:$HOME/mylatex:$HOME/mylatex/tikz-dsp:$HOME/mylatex/coverletter:$TEXINPUTS"
-set -gx BIBINPUTS "$HOME/mylatex:$BIBINPUTS"
-set -gx BSTINPUTS "$HOME/mylatex:$BSTINPUTS"
-set -gx MANPATH (manpath -q)
-set -gx MANPATH "$MANPATH:$HOME/man"
-
-# Load in host specific environment variables
-switch (hostname)
-case yoshi
-    source $HOME/.config/fish/alias_vars_yoshi.fish
-case mario
-    source $HOME/.config/fish/alias_vars_mario.fish
-case hazza 
-    source $HOME/.config/fish/alias_vars_hazza.fish
-case Glen 
-    source $HOME/.config/fish/alias_vars_glen.fish
-case *
-    source $HOME/.config/fish/alias_vars_blank.fish
-end
+eval (dircolors /home/fergal/repos/seebi/dircolors-solarized/dircolors.256dark | head -n 1 | sed 's/^LS_COLORS=/set -x LS_COLORS /;s/;$//')
+source $HOME/.config/fish/alias_vars.fish
 
 function pycscope
     find . -path './venv' -prune -o -name '*.py*' -print > cscope.files
     cscope -bR
 end
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/scratch/fbc23/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/scratch/fbc23/google-cloud-sdk/path.fish.inc'; else; . '/scratch/fbc23/google-cloud-sdk/path.fish.inc'; end; end
+bass source /etc/profile.d/apps-bin-path.sh
